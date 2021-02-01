39467f050abf28ce34d150b3b5a101706681f0e8
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-02-01 17:22:57 +0200

    добавлен хеддер
    вырезала разметку с index.html и вставила в modal.hbs

---
+++ src/js/refs.js
@@ -0,0 +1,11 @@
+export const refs = {
+  refsInput: document.getElementById('input'),
+  navRefs: document.getElementById('nav'),
+  formRef: document.querySelector('.form-search'),
+  bntLibraryRef: document.querySelector('.library-btn'),
+  linkHome: document.querySelector('.home'),
+  linkLibrary: document.querySelector('.library'),
+  headerImgRef: document.querySelector('.header-img'),
+  LogoRefs: document.querySelector('.logo-home'),
+  GalleryRefs: document.querySelector('.list-library'),
+};
