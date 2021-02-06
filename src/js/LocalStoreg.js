af142e955f1c9df97fa504d5bcd81e892e2e83e7
NataliBorshch <67202688+NataliBorshch@users.noreply.github.com>
2021-02-06 18:21:16 +0200

    добавление у удаление LocalStr

--- src/js/LocalStoreg.js
+++ src/js/LocalStoreg.js
@@ -1,34 +1,4 @@
 
-export function localStorageW(event,film , arrayIdW ,btnWatch){
-    let indexW = 0;
-    if (event.target.textContent === 'remove to watch'){
-        console.log('Надо удалить такое уже есть ')
-        getIndex(arrayIdW, film , btnWatch)
-    }
 
-    else{
-        arrayIdW.push(film)
-        let watchedStr = JSON.stringify(arrayIdW)
-        event.target.textContent = 'remove to watch';
-        localStorage.setItem(`w`, watchedStr)
-    }
-
-}
-
-
-
-function getIndex(arrayIdW, film,btnWatch){
-    console.log(arrayIdW)
-    let indexW = 0;
     
-    arrayIdW.forEach((elem,index) => {
-        if (elem.id === film.id ){
-            console.dir(btnWatch)
-            btnWatch.textContent = 'remove to watch';
-            indexW = index;  
-        }
-        return indexW;
-    });
 
- console.log(indexW)
-}
--- src/js/filmDetailsPage.js
+++ src/js/filmDetailsPage.js
@@ -1,18 +1,12 @@
 import { refs } from './refs';
 import TemplateDetailPage from '../templates/modal.hbs';
 import {eventKeyDown , closeModal } from './closeModal';
-import {localStorageW} from './LocalStoreg';
 
 
-// console.log(refs);
-// const arrayIdQ = JSON.parse(localStorage.getItem(`queue`)) || [];
- let arrayIdW = [];
 
-
-// console.log(arrayIdQ)
-// const getQueue = localStorage.getItem('queue');
-// arrayIdQ.push(Number(getQueue));
-
+let parseW =  JSON.parse(localStorage.getItem('watch')) || [] ;
+// console.log(parseW)
+let parseQ = JSON.parse(localStorage.getItem('queue')) || [];
 
 async function getdetailsPage(id) {
   // event.preventDefault();
@@ -23,6 +17,7 @@ async function getdetailsPage(id) {
   const film = await response.json();
   const templateCardFilm = await TemplateDetailPage(film);
   const createCardFilm = await createDatails(refs.modalRefs, templateCardFilm);
+  
   refs.modalRefs.classList.remove('is-hidden');
   refs.modalRefs.addEventListener('click', closeModal);
   document.addEventListener('keydown', eventKeyDown);
@@ -31,11 +26,47 @@ async function getdetailsPage(id) {
     queue: document.getElementById('queue-add'),
     boxBtn: document.getElementById('modal-box-bnt'),
   }; 
-  modalBtn.watch.addEventListener('click',event=>{
-    event.preventDefault()
-    localStorageW(event,film, arrayIdW, modalBtn.watch )
 
+
+  // кнопка добавитьв просмотренные W
+  const IndexItemW  = await ChangeTextBtn(parseW,film,modalBtn.watch);
+  const IndexItemQ  = await ChangeTextBtn(parseW,film,modalBtn.queue);
+  modalBtn.watch.addEventListener('click',event=>{
+    event.preventDefault()   
+    if (event.target.textContent === 'remove to watch'){
+       parseW.splice(IndexItemW, 1)
+      console.log(parseW)
+      localStorage.setItem('watch', JSON.stringify(parseW))
+      ChangeTextBtn(parseW, film, modalBtn.watch)
+      
+    }
+    else{
+      parseW.push(film)
+      ChangeTextBtn(parseW, film, modalBtn.watch)
+      localStorage.setItem('watch', JSON.stringify(parseW))
+    }
   });
+
+//  кнопка добавитьв Q
+  modalBtn.queue.addEventListener('click',event=>{
+    event.preventDefault()   
+    if (event.target.textContent === 'remove to queue'){
+       parseQ.splice(IndexItemQ, 1)
+      console.log(parseQ)
+      localStorage.setItem('queue', JSON.stringify(parseQ))
+      ChangeTextBtnQ(parseQ, film, modalBtn.queue)
+     
+    }
+    else{
+      parseQ.push(film)
+      ChangeTextBtnQ(parseQ, film, modalBtn.queue)
+      localStorage.setItem('queue', JSON.stringify(parseQ))
+    }
+  });
+
+
+
+  // console.log(parseW)
 }
 
 
@@ -46,12 +77,60 @@ async function getdetailsPage(id) {
 
 
 
+// находим индекс елемента который есть в локар сторедж для watch
+
+function ChangeTextBtn(parseJson, film,btnWatch){
+  if (parseJson.length < 1){
+    btnWatch.textContent = 'add to Watched'
+    return;
+  }
+  let indexW = 0;
+  parseJson.forEach((elem,index) => {
+      if (elem.id === film.id ){
+          btnWatch.textContent = 'remove to watch';
+          indexW = index;  
+      }
+      else{
+        btnWatch.textContent = 'add to Watched'
+      }
+  });
+  return indexW;
+}
+
+
+
+// для Q
+
+function ChangeTextBtnQ(parseJson, film,btn){
+  if (parseJson.length < 1){
+    btn.textContent = 'add to queue';
+    return;
+  }
+  let indexQ = 0;
+
+  parseJson.forEach((elem,index) => {
+      if (elem.id === film.id ){
+        btn.textContent = 'remove to queue';
+          indexQ = index;  
+      }
+      else{
+        btn.textContent = 'add to queue'
+      }
+  });
+
+//  console.log(indexW)
+
+  return indexQ;
+}
 
 
 
 
 
 
+
+
+// создаем разметку 
 
 function createDatails(place, tepmlate) {
   return place.insertAdjacentHTML('beforeend', tepmlate);
